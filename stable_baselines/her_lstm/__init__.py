9536f1106182c1e9ef7c84cbc2ccb28fc6ead574
kevslinger <siouxman@me.com>
2021-01-08 15:37:26 -0500

    added dqn and her directories for DRQN

---
+++ stable_baselines/her_lstm/__init__.py
@@ -0,0 +1,3 @@
+from stable_baselines.her_lstm.her import HERRQN
+from stable_baselines.her_lstm.replay_buffer import GoalSelectionStrategy, HindsightExperienceReplayWrapper
+from stable_baselines.her_lstm.utils import HERGoalEnvWrapper
